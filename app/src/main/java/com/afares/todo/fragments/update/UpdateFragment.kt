dda956cd083a174f9fc2d27232806c5a7c5667e6
AFares1 <ahmedfaresa10@gmail.com>
2021-01-17 00:47:49 +0200

    first commit init app with navigation components UI

---
+++ app/src/main/java/com/afares/todo/fragments/update/UpdateFragment.kt
@@ -0,0 +1,26 @@
+package com.afares.todo.fragments.update
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.afares.todo.R
+
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_update, container, false)
+
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
+}
