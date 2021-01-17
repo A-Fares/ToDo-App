d3189d607a667a40162df46cf05f1789bf3b9441
AFares1 <ahmedfaresa10@gmail.com>
2021-01-17 10:37:18 +0200

    init commit Create Navigation Components with UI

---
+++ app/src/main/java/com/afares/todo/fragments/add/AddFragment.kt
@@ -0,0 +1,25 @@
+package com.afares.todo.fragments.add
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.afares.todo.R
+
+class AddFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        setHasOptionsMenu(true)
+        return view;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+}
---
+++ app/src/main/java/com/afares/todo/fragments/list/ListFragment.kt
@@ -0,0 +1,36 @@
+package com.afares.todo.fragments.list
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.afares.todo.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        view.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+}
